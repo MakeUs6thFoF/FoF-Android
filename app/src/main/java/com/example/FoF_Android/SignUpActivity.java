1ca61ab13c484c259c2e8a35b1769d6e5c0704a2
HongEunho <ghdwns315@naver.com>
2021-02-21 18:27:59 +0900

    update Activity

--- app/src/main/java/com/example/FoF_Android/SignUpActivity.java
+++ app/src/main/java/com/example/FoF_Android/SignUpActivity.java
@@ -9,8 +9,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class SignUpActivity extends AppCompatActivity {
     Button signup_btn;
@@ -19,6 +25,8 @@ public class SignUpActivity extends AppCompatActivity {
     EditText ed_email;
     EditText ed_pswd;
     EditText ed_nick;
+
+    RetrofitApi api;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,8 @@ public class SignUpActivity extends AppCompatActivity {
         ed_pswd = findViewById(R.id.ed_pswd);
         ed_nick = findViewById(R.id.ed_nick);
 
+        api = HttpClient.getRetrofit().create(RetrofitApi.class);
+
         login_link.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,13 +52,15 @@ public class SignUpActivity extends AppCompatActivity {
         signup_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 String email = ed_email.getText().toString();
                 String pswd = ed_pswd.getText().toString();
                 String nickname = ed_nick.getText().toString();
 
                 if (!checkEmail(email)) tv_email_check.setVisibility(View.VISIBLE);
-                else tv_email_check.setVisibility(View.GONE);
+                else{
+                    tv_email_check.setVisibility(View.GONE);
+                    doSignUp(email, pswd, nickname, api);
+                }
                 //TODO 데이터 전달
             }
         });
@@ -63,6 +75,28 @@ public class SignUpActivity extends AppCompatActivity {
         boolean isNormal = m.matches();
         return isNormal;
 
+    }
+
+    public void doSignUp(String email, String pswd, String nickname, RetrofitApi api){
+        HashMap<String, Object> input = new HashMap<>();
+        input.put("email", email);
+        input.put("password", pswd);
+        input.put("nickname", nickname);
+        api.postSignUp(input).enqueue(new Callback<SignUp>() {
+            @Override
+            public void onResponse(Call<SignUp> call, Response<SignUp> response) {
+                if (response.isSuccessful()){
+                    System.out.println("포스트 성공1");
+                }
+                else
+                    System.out.println(response.message());
+            }
+
+            @Override
+            public void onFailure(Call<SignUp> call, Throwable t) {
+                System.out.println("실패");
+            }
+        });
     }
 
 }
