6041c103ac423ae1dd52f84b3836872c02f5f375
HongEunho <ghdwns315@naver.com>
2021-02-26 23:23:09 +0900

    Create CongActivity.java

---
+++ app/src/main/java/com/example/FoF_Android/CongActivity.java
@@ -0,0 +1,14 @@
+package com.example.FoF_Android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cong);
+    }
+}
