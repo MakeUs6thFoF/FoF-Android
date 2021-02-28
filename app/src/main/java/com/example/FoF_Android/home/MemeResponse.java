29fc37ab7ec3135fc477a988271e1b608fc774ac
HSHyeon <hongsohyun86@gmail.com>
2021-03-01 04:10:26 +0900

    Update HomeFragment

---
+++ app/src/main/java/com/example/FoF_Android/home/MemeResponse.java
@@ -0,0 +1,21 @@
+package com.example.FoF_Android.home;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MemeResponse {
+
+    @SerializedName("data")
+    @Expose
+    private List<Meme.Data> items;
+
+    public List<Meme.Data> getItems(){
+        return items;
+    }
+    public void setItems(List<Meme.Data>items){
+        this.items = items;
+    }
+}
+
