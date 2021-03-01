00932407a75e3a1b41eec582314c07fc12ce4d23
HongEunho <ghdwns315@naver.com>
2021-03-01 14:22:36 +0900

    Create SearchFragment.java

---
+++ app/src/main/java/com/example/FoF_Android/search/SearchFragment.java
@@ -0,0 +1,69 @@
+package com.example.FoF_Android.search;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.FoF_Android.R;
+import com.google.android.material.tabs.TabLayout;
+
+public class SearchFragment extends Fragment {
+    TabLayout tabLayout;
+    Fragment feeling;
+    Fragment animal;
+    Fragment situation;
+    Fragment clip;
+    Fragment text;
+    Fragment emoticon;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+    public static SearchFragment newInstance(String param1, String param2) {
+        SearchFragment fragment = new SearchFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        feeling = new FeelingFragment();
+        animal = new AnimalFragment();
+        situation = new SituationFragment();
+        clip = new ClipFragment();
+        text = new TextFragment();
+        emoticon = new EmoticonFragment();
+
+        return view;
+    }
+}
