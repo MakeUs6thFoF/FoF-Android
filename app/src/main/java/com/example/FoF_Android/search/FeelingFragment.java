f669c682687298be0ceb7d4edbd26a0e89da277a
HongEunho <ghdwns315@naver.com>
2021-03-01 14:23:11 +0900

    Create FeelingFragment.java

---
+++ app/src/main/java/com/example/FoF_Android/search/FeelingFragment.java
@@ -0,0 +1,51 @@
+package com.example.FoF_Android.search;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.FoF_Android.R;
+
+
+public class FeelingFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FeelingFragment() {
+        // Required empty public constructor
+    }
+
+    public static FeelingFragment newInstance(String param1, String param2) {
+        FeelingFragment fragment = new FeelingFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_feeling, container, false);
+    }
+}
