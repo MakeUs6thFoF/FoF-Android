3e38a79d25483cfe28aa72e349b2e4452535af30
HongEunho <ghdwns315@naver.com>
2021-03-17 07:00:03 +0900

    update

--- app/src/main/java/com/example/FoF_Android/search/SearchFragment.java
+++ app/src/main/java/com/example/FoF_Android/search/SearchFragment.java
@@ -3,6 +3,7 @@ package com.example.FoF_Android.search;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
@@ -10,27 +11,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
+import android.widget.TextView;
 
+import com.example.FoF_Android.HttpClient;
 import com.example.FoF_Android.R;
+import com.example.FoF_Android.RetrofitApi;
+import com.example.FoF_Android.TokenManager;
 import com.google.android.material.tabs.TabLayout;
 import com.ramotion.expandingcollection.ECCardData;
 import com.ramotion.expandingcollection.ECPagerView;
 
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class SearchFragment extends Fragment {
     TabLayout tabLayout;
     NonSwipeViewPager viewPager;
+    RetrofitApi api;
+    TokenManager gettoken;
+    TextView hash1;
+
+    int tagIdx[] = new int[5];
+    String tagName[] = new String[5];
+    int searchCnt[] = new int[5];
 
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -39,8 +47,6 @@ public class SearchFragment extends Fragment {
     public static SearchFragment newInstance(String param1, String param2) {
         SearchFragment fragment = new SearchFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -48,10 +54,8 @@ public class SearchFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        api = HttpClient.getRetrofit().create(RetrofitApi.class);
+        gettoken = new TokenManager(getContext());
     }
 
     @Override
@@ -59,7 +63,8 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_search, container, false);
-
+        hash1 = view.findViewById(R.id.hash1);
+        getHashTag(api);
 
         tabLayout = (TabLayout)view.findViewById(R.id.searchTabLayout);
         tabLayout.addTab(tabLayout.newTab().setText("감정"));
@@ -84,9 +89,31 @@ public class SearchFragment extends Fragment {
             public void onTabReselected(TabLayout.Tab tab) { }
         });
 
-        LinearLayout linearLayout = (LinearLayout)view.findViewById(R.id.bottomLayout);
-        
+
+        RecyclerView mRecyclerView = view.findViewById(R.id.hashtag_recycler);
+
 
         return view;
+    }
+
+    public void getHashTag(RetrofitApi api){
+        String token = gettoken.checklogin(getContext());
+        api.getTag(token).enqueue(new Callback<HashTag>() {
+            @Override
+            public void onResponse(Call<HashTag> call, Response<HashTag> response) {
+                HashTag tag = response.body();
+                for(int i=0; i<tag.getData().getTagList().size(); i++){
+                    tagIdx[i] = tag.getData().getTagList().get(i).getTagIdx();
+                    tagName[i] = tag.getData().getTagList().get(i).getTagName();
+                    searchCnt[i] = tag.getData().getTagList().get(i).getSearchCnt();
+                }
+                hash1.setText(tagName[0]);
+            }
+
+            @Override
+            public void onFailure(Call<HashTag> call, Throwable t) {
+
+            }
+        });
     }
 }
