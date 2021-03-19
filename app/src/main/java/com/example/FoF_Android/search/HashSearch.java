88962586b4a996464810322582d68a05ffbf9abd
HongEunho <ghdwns315@naver.com>
2021-03-19 13:42:15 +0900

    Create HashSearch.java

---
+++ app/src/main/java/com/example/FoF_Android/search/HashSearch.java
@@ -0,0 +1,34 @@
+package com.example.FoF_Android.search;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class HashSearch {
+
+    @SerializedName("data") private Data data;
+    private boolean isSuccess;
+    private int code;
+    private String message;
+
+    public class Data{
+        @SerializedName("memeCount") int memeCount;
+        @SerializedName("memeList") List<memeList> memeList;
+
+        public class memeList{
+            @SerializedName("memeIdx") int memeIdx;
+            @SerializedName("imageUrl") String imageUrl;
+
+            public int getMemeIdx() { return memeIdx;  }
+            public String getImageUrl() { return imageUrl;  }
+        }
+
+        public List<memeList> getMemeList() { return memeList; }
+        public int getMemeCount() {return memeCount;}
+    }
+
+    public Data getData() {return data;}
+    public boolean isSuccess() { return isSuccess; }
+    public int getCode() {return code; }
+    public String getMessage() {return message; }
+}
