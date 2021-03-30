dbfc3f0df54ccf4338d439578f6575d00ae2221f
HongEunho <ghdwns315@naver.com>
2021-03-30 14:53:36 +0900

    Update LoginActivity.java

--- app/src/main/java/com/example/FoF_Android/login/LoginActivity.java
+++ app/src/main/java/com/example/FoF_Android/login/LoginActivity.java
@@ -110,6 +110,9 @@ public class LoginActivity extends AppCompatActivity {
                         Toast.makeText(getApplicationContext(), "탈퇴 된 계정입니다", Toast.LENGTH_SHORT).show();
                     else if(flag == 301)
                         Toast.makeText(getApplicationContext(), "이메일을 입력해주세요", Toast.LENGTH_SHORT).show();
+                    else if(flag == 311){
+                        Toast.makeText(getApplicationContext(), "비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show();
+                    }
                 }
                 else
                     System.out.println(response.toString());
